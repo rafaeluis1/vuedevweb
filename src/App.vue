e7d930811f1b0aa683a29fd43b5873c63cde4b04
Rafael Santos <rafael.santos@globalsys.com.br>
2021-11-25 14:24:17 -0300

    Rotas definidas

--- src/App.vue
+++ src/App.vue
@@ -7,30 +7,31 @@
 
     <v-app-bar app clipped-left>
       <v-row justify="center">
-        <v-btn v-for="link in links" :key="link" text>
-          {{ link }}
+        <v-btn v-for="link in links" :key="link.title" :to="link.to" text>
+          {{ link.title }}
         </v-btn>
       </v-row></v-app-bar
     >
 
     <v-navigation-drawer app clipped left>
       <v-list>
-        <v-list-item v-for="n in 5" :key="n" link>
+        <v-list-item v-for="link in links" :key="link.title" :to="link.to" link>
           <v-list-item-content>
-            <v-list-item-title>Item {{ n }}</v-list-item-title>
+            <v-list-item-title>{{ link.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
 
     <v-main>
-      <v-container fill-height>
+      <router-view></router-view>
+      <!-- <v-container fill-height>
         <v-row justify="center" align="center">
           <v-col cols="12" sm="4">
             <h4>v-content</h4>
           </v-col>
         </v-row>
-      </v-container>
+      </v-container> -->
     </v-main>
 
     <v-footer app padless>
@@ -48,7 +49,12 @@
 export default {
   data: () => ({
     drawer: null,
-    links: ["Dashboard", "Messages", "Profile", "Updates"],
+    links: [
+      { title: "Home", to: "/" },
+      { title: "Sign-up", to: "/signup" },
+      { title: "About", to: "/about" },
+      { title: "Contact", to: "/contact" },
+    ],
   }),
 };
 </script>
